aa69f25c4fccf2e0da0bd5bf77ef5eb868feae41
Marysia Winkels <10163727@u030252.fnwi.uva.nl>
2012-10-11 10:57:08 +0200

    sdfja

--- proxy.c
+++ proxy.c
@@ -4,7 +4,6 @@
  * TEAM MEMBERS:
  *     Jouke van der Maas, 10186883, jouke.vandermaas@gmail.com 
  *     Marysia Winkels, 10163727, marysia@live.nl
- dsafds
  * 
  * IMPORTANT: Give a high level description of your code here. You
  * must also provide a header comment at the beginning of each
@@ -24,13 +23,28 @@ void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, char *uri, 
  */
 int main(int argc, char **argv)
 {
-
+    int clientfd, port; 
+    char *host, buf[MAXLINE];
+    rio_t rio; 
+    
     /* Check arguments */
     if (argc != 2) {
 	fprintf(stderr, "Usage: %s <port number>\n", argv[0]);
 	exit(0);
     }
-
+    
+    host = argv[2]; 
+    port - atoi(argv[1]);
+    
+    clientfd = Open_clientfd(host, port);
+    Rio_readinitb(&rio, clientfd);
+    
+    while(Fgets(buf, MAXLINE, stdin) != NULL) {
+      Rio_writen(clientfd, buf, strlen(buf));
+      Rio_readlineb(&rio, buf, MAXLINE);
+      Fputs(buf, stdout);
+    }
+    Close(clientfd);
     exit(0);
 }
 
@@ -68,7 +82,7 @@ int parse_uri(char *uri, char *hostname, char *pathname, int *port)
 	*port = atoi(hostend + 1);
     
     /* Extract the path */
-    pathbegin = strchr(hostbegin, '/');
+    pathbegin = strchr(hos-tbegin, '/');
     if (pathbegin == NULL) {
 	pathname[0] = '\0';
     }
